a09a420db702ffaf3eb50bbbd2e6b5f67fdb5c3e
Abrar Mehdi <abrar@browserstack.com>
2022-06-10 18:27:08 +0530

    Modified the code for the demonstration of test ng parallel test cases

--- src/test/java/com/browserstack/BrowserStackTestNGTest.java
+++ src/test/java/com/browserstack/BrowserStackTestNGTest.java
@@ -2,75 +2,58 @@ package com.browserstack;
 
 import java.io.FileReader;
 import java.net.URL;
+import java.sql.SQLOutput;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 import com.browserstack.local.Local;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class BrowserStackTestNGTest {
+    
+    private static final String URL = "hub-cloud.browserstack.com";
     public WebDriver driver;
     private Local l;
 
     @BeforeMethod(alwaysRun = true)
-    @org.testng.annotations.Parameters(value = { "config", "environment" })
-    @SuppressWarnings("unchecked")
-    public void setUp(String config_file, String environment) throws Exception {
-        JSONParser parser = new JSONParser();
-        JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
-        JSONObject envs = (JSONObject) config.get("environments");
-
+    @Parameters(value = {"deviceName", "osVersion", "browserName", "username", "key"})
+    public void setup(String deviceName, String osVersion, String browserName, String username,
+                                    String key) throws Exception {
+    
+        System.out.println("Device Name Hit "+deviceName);
         DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        Map<String, String> envCapabilities = (Map<String, String>) envs.get(environment);
-        Iterator it = envCapabilities.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry) it.next();
-            capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
-        }
-
-        Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
-        it = commonCapabilities.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry) it.next();
-            if (capabilities.getCapability(pair.getKey().toString()) == null) {
-                capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
-            }
-        }
-
-        String username = System.getenv("BROWSERSTACK_USERNAME");
-        if (username == null) {
-            username = (String) config.get("user");
-        }
-
-        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-        if (accessKey == null) {
-            accessKey = (String) config.get("key");
-        }
-
-        if (capabilities.getCapability("browserstack.local") != null
-                && capabilities.getCapability("browserstack.local") == "true") {
-            l = new Local();
-            Map<String, String> options = new HashMap<String, String>();
-            options.put("key", accessKey);
-            l.start(options);
-        }
-
+        HashMap<String, Object> bstackOptions = new HashMap<>();
+        
+        bstackOptions.put("deviceName", deviceName);
+        bstackOptions.put("browserName", browserName);
+        bstackOptions.put("osVersion", osVersion);
+        bstackOptions.put("realMobile", "true");
+        bstackOptions.put("appiumVersion", "1.22.0");
+        bstackOptions.put("projectName", "Sample ");
+        bstackOptions.put("buildName",
+                          "Parallel Test "+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm:ss")));
+        
+        capabilities.setCapability("bstack:options", bstackOptions);
+    
+    
         driver = new RemoteWebDriver(
-                new URL("http://" + username + ":" + accessKey + "@" + config.get("server") + "/wd/hub"), capabilities);
+                new URL("http://" + username + ":" + key + "@" + URL + "/wd/hub"), capabilities);
     }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() throws Exception {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
         if (l != null) {
             l.stop();
         }
